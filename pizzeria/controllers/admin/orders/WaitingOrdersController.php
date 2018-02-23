3aa9b88c215364becb897783660f66b1a069915d
marcinseweryn <marcin-seweryn@wp.pl>
2018-02-24 00:23:55 +0100

    Added new orders notifications

--- pizzeria/controllers/admin/orders/WaitingOrdersController.php
+++ pizzeria/controllers/admin/orders/WaitingOrdersController.php
@@ -3,6 +3,7 @@
 class WaitingOrdersController{
     
     public function generateView(){
+
     
         getSmarty()->display(getConf()->root_path.'/views/admin/orders/waiting-orders.html');
     }
